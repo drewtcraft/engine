fb2cf8a91c9d7e5d4457de3288029a4ed4db2ef7
andrew <drewtcraft@gmail.com>
2020-06-07 16:34:56 -0400

    kill target dir

--- target/debug/deps/pmwb-008097fa262cee27.d
+++
@@ -1,5 +0,0 @@
-/home/andrew/engine/target/debug/deps/pmwb-008097fa262cee27: src/main.rs
-
-/home/andrew/engine/target/debug/deps/pmwb-008097fa262cee27.d: src/main.rs
-
-src/main.rs:
--- target/debug/pmwb.d
+++
@@ -1,1 +0,0 @@
-/home/andrew/engine/target/debug/pmwb: /home/andrew/engine/src/main.rs
