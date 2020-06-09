0970ad20830250b04eca562e6cf5d3feb9a6f085
andrew <drewtcraft@gmail.com>
2020-06-09 18:13:41 -0400

    rethink last commit

--- src/util/model.rs
+++ src/util/model.rs
@@ -9,3 +9,14 @@ pub struct Rect {
 	pub bottom_right: Point,
 	pub bottom_left: Point,
 }
+
+pub struct Color(u8, u8, u8);
+
+pub struct Visible {
+	pub body: Vec<(Point, Color)>,
+}
+
+pub struct Mass {
+	pub body: Vec<(Point, Color)>,
+	pub perimeter: Vec<Point>,
+}
