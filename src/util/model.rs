699cdcaf811ceb782cb0656257f5cc5ea721c068
andrew <drewtcraft@gmail.com>
2020-06-10 21:33:17 -0400

    start on tracing algorithm

--- src/util/model.rs
+++ src/util/model.rs
@@ -24,6 +24,38 @@ pub struct Visible {
 pub struct Mass {
 	pub body: [ [ Pixel; 20 ]; 20 ], 
 	pub perimeter: Vec<Point>,
+	perimeter_reference_point: Point,
+}
+
+pub struct Coord(u8, u8);
+
+// direction enum, letters represent cardinal directions
+pub enum Dir {
+	E,
+	NE,
+	N,
+	NW,
+	W,
+	SW,
+	S,
+	SE,
+}
+
+fn get_neighbor (direction: Dir, coord: Coord) -> Coord {
+	match direction {
+		Dir::E => Coord(coord.0 - 1, coord.1),
+		Dir::NE => Coord(coord.0 - 1, coord.1 + 1),
+		Dir::N => Coord(coord.0, coord.1 + 1),
+		Dir::NW => Coord(coord.0 + 1, coord.1 + 1),
+		Dir::W => Coord(coord.0 + 1, coord.1),
+		Dir::SW => Coord(coord.0 + 1, coord.1 - 1),
+		Dir::S => Coord(coord.0, coord.1 - 1),
+		Dir::SE => Coord(coord.0 - 1, coord.1 - 1),
+	}
+}
+
+fn trace_perimeter (body: [[Pixel;20];20], point: Coord, last_coord: Coord) {
+	
 }
 
 impl Mass {
