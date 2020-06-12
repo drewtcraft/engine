dad79566fef937b7494c8aadf8918cf5fc4e0ddb
andrew <drewtcraft@gmail.com>
2020-06-12 14:53:23 -0400

    Revert "fuck I am bad at this"
    This reverts commit 6779729e7837ec150756a25c084c4857e004ea73.

--- src/util/model.rs
+++ src/util/model.rs
@@ -18,13 +18,13 @@ pub struct Pixel {
 }
 
 pub struct Visible {
-	pub body: [ [ Option<Color>; 20 ]; 20 ], 
+	pub body: [ [ Pixel; 20 ]; 20 ], 
 }
 
 pub struct Coord(u8, u8);
 
 pub struct Mass {
-	pub body: [ [ Option<Color>; 20 ]; 20 ], 
+	pub body: [ [ Pixel; 20 ]; 20 ], 
 	pub perimeter: Vec<Coord>,
 	perimeter_reference_point: Coord,
 }
@@ -41,57 +41,42 @@ pub enum Dir {
 	SE,
 }
 
-// make sure the coord is not out of bounds
-// if it is return None
-fn get_coord_neighbor (coord: &Coord, diff: (i16, i16)) -> Option<Coord> {
-	let x = coord.0 + diff.0;
-	let y = coord.1 + diff.1;
-	if x >= 0 && x <= 20 && y >= 0 && y <= 20 {
-		Some(Coord(x, y))
-	}
-	else { None }
-}
-
 // just tells us how to move, given the direction
-fn get_coord_from_direction (direction: &Dir, coord: &Coord) -> Option<Coord> {
-	let next:(i16, i16) = match direction {
-		Dir::E => 	(-1, 0),
-		Dir::NE => 	(-1, 1),
-		Dir::N => 	(0, 1),
-		Dir::NW => 	(1, 1),
-		Dir::W => 	(1, 0),
-		Dir::SW => 	(1, -1),
-		Dir::S => 	(0, -1),
-		Dir::SE => 	(-1, -1),
-	}
-	get_coord_neighbor(coord, next)
-}
-
-fn get_next_direction (direction: Option<&Dir>) -> Dir {
+fn get_2d_neighbor (direction: Dir, coord: Coord) -> Coord {
 	match direction {
-		Some(dir) => {
-			match dir {
-				Dir::E => Dir::NE,
-				Dir::NE => Dir::N,
-				Dir::N => Dir::NW,
-				Dir::NW => Dir::W,
-				Dir::W => Dir::SW,
-				Dir::SW => Dir::S,
-				Dir::S => Dir::SE,
-				Dir::SE => Dir::E,
-			}
-		},
-		None => Dir::E,
+		Dir::E => Coord(coord.0 - 1, coord.1),
+		Dir::NE => Coord(coord.0 - 1, coord.1 + 1),
+		Dir::N => Coord(coord.0, coord.1 + 1),
+		Dir::NW => Coord(coord.0 + 1, coord.1 + 1),
+		Dir::W => Coord(coord.0 + 1, coord.1),
+		Dir::SW => Coord(coord.0 + 1, coord.1 - 1),
+		Dir::S => Coord(coord.0, coord.1 - 1),
+		Dir::SE => Coord(coord.0 - 1, coord.1 - 1),
 	}
 }
 
-fn trace_2d_perimeter (body: [[Option<Color>;20];20], coord: Coord, last_dir: Option<Dir>) -> Vec<Coord> {
-	let direction: Dir = get_next_direction(last_dir);
+fn get_next_direction (direction: Dir) -> Dir {
+	match direction {
+		Dir::E => Dir::NE,
+		Dir::NE => Dir::N,
+		Dir::N => Dir::NW,
+		Dir::NW => Dir::W,
+		Dir::W => Dir::SW,
+		Dir::SW => Dir::S,
+		Dir::S => Dir::SE,
+		Dir::SE => Dir::E,
+	}
+}
 
-	let mut next_pixel: Option<Pixel> = None;
-	let mut found_empty = false;
+fn trace_2d_perimeter (body: [[Pixel;20];20], coord: Coord, last_dir: Option<Dir>) -> Vec<Coord> {
+	let direction: Dir = match last_dir {
+		Some(d) => get_next_direction(d),
+		None => Dir::E
+	};
+
+	let mut next_coord: Option<Coord> = None;
 	while let None = next_coord {
-		let (x, y) = get_coord_from_direction(direction, coord);
+		// do something
 	}
 
 	vec![]
